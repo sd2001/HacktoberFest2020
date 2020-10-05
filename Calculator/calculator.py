de4d86bbcd9c8efd2600cb25fdf5f79aa910ea1c
Vismaya K <60786033+VISMAYA-K@users.noreply.github.com>
2020-10-05 09:41:29 +0530

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,13 +1,14 @@
 # Simple Calculator
-import math
 def add(a, b):
-    return (a + b)
+    return a + b
 def sub(a, b):
-    return (a - b)
+    return a - b
 def mult(a, b):
-    return (a * b)
+    return a * b
 def div(a, b):
-    return (a / b)
+    return a / b
+def mod(a, b):
+    return a%b
 
 x = int(input("First number: "))
 y = int(input("Second number: "))
@@ -16,6 +17,7 @@ print("\n1 - add")
 print("2 - subtract")
 print("3 - multiply")
 print("4 - divide")
+print("5 - Remainder")
 
 choice = int(input("Choose: "))
 
@@ -27,3 +29,7 @@ elif (choice == 3):
     print(mult(x,y))
 elif (choice == 4):
     print(div(x,y))
+elif (choice==5):
+    print(mod(x,y))
+else:
+    print("Invalid Choice!")
