6458b184631a627b477f7be3c4f62dea2e3d621b
MisbahudDin <72780676+Misbahud-Din@users.noreply.github.com>
2020-10-14 14:05:16 +0500

    Added Colored_console_in_cpp

---
+++ Colored_console_in_cpp/main.cpp
@@ -0,0 +1,29 @@
+/*
+Author:		Misbahud Din
+Language:	C++
+Project:	Coloured Console
+Description:	Everyone Likes colors :) . This program shows some of the colors c++ can display in console
+*/
+
+
+#include <Windows.h>		// Header file for colors
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	string name;
+	cout << "Enter your name: ";
+	cin >> name;
+
+	for (int i = 0; i <+ 255; i++)
+	{
+		SetConsoleTextAttribute(h, i);
+		cout << "Your name is " << name << endl;
+	}
+	
+	return 0;
+}
