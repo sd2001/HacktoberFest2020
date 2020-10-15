4c0599207b0f866601f7b7ceccf3e2ea25e928df
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2020-10-16 00:08:35 +0530

    Added Meme Generator

---
+++ meme-generator/src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Header() {
+  return (
+    <header>
+      {/* <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem?" /> */}
+      <p>Meme Generator</p>
+    </header>
+  );
+}
+export default Header;
